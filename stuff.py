d68a2d1faecdedab3f5abe22242765f03d16e405
Jacob Zirbel <jacobzirbel@gmail.com>
2020-12-05 17:52:29 -0600

    second

--- stuff.py
+++ stuff.py
@@ -1,20 +1,24 @@
 import requests
-import RPi.GPIO as G
+import RPi.GPIO as GPIO
 import time
 
 pin = 4
-G.setmode(G.BCM)
-G.setup(pin, G.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
 
 key = "44bac61c7ab1d1fef3a2b0820afdc093"
+lat = '43.0832242'
+lon = '-89.3815317'
 city = "madison"
 state = "WI"
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city},{state}&appid={key}'
+url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={key}'
 print(url)
-response = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=43.0731&lon=-89.4012&units=imperial&appid=44bac61c7ab1d1fef3a2b0820afdc093').json()
+response = requests.get(url).json()
 print(response['weather'][0]['description'])
 print(response['main']['temp'])
 if True:
-    G.output(pin, False)
+    GPIO.output(pin, True)
     time.sleep(1)
+    GPIO.output(pin, False)
 
+GPIO.cleanup()
